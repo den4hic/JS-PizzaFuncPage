2858d681383e94fb47754d4ecafb713f0bedc958
Denys <gusardavydov2004@gmail.com>
2023-06-20 19:43:20 +0300

    JSON update done, sort left

--- src/script.js
+++ src/script.js
@@ -1,3 +1,247 @@
+fetch('../src/Pizza.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Помилка при виконанні запиту');
+        }
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(item => createPizza(item))
+    })
+    .catch(error => {
+        console.error('Помилка:', error);
+    });
+
+function createPizza(data) {
+    const pizzaCard = document.createElement("div")
+    pizzaCard.className = "pizza-card"
+
+    if (data.is_new && data.is_popular) {
+        const twoBadgesDiv = document.createElement("div")
+        twoBadgesDiv.className = "all-badges"
+
+        const newDiv = document.createElement("div")
+        newDiv.className = "pizza-badge"
+        newDiv.style = "background: #DB524B"
+
+        const newText = document.createElement("strong")
+        newText.textContent = "Нова"
+
+        newDiv.appendChild(newText)
+        twoBadgesDiv.appendChild(newDiv)
+
+        const popularDiv = document.createElement("div")
+        popularDiv.className = "pizza-badge"
+        popularDiv.style = "background: #59B957"
+
+        const popularText = document.createElement("strong")
+        popularText.textContent = "Популярна"
+
+        popularDiv.appendChild(popularText)
+        twoBadgesDiv.appendChild(popularDiv)
+        pizzaCard.appendChild(twoBadgesDiv)
+    } else if (data.is_new) {
+        const newDiv = document.createElement("div")
+        newDiv.className = "pizza-badge"
+        newDiv.style = "background: #DB524B; position: absolute;\n" +
+            "    top: -10px;\n" +
+            "    right: -6px;"
+
+        const newText = document.createElement("strong")
+        newText.textContent = "Нова"
+
+        newDiv.appendChild(newText)
+        pizzaCard.appendChild(newDiv)
+    } else if (data.is_popular) {
+        const popularDiv = document.createElement("div")
+        popularDiv.className = "pizza-badge"
+        popularDiv.style = "background: #59B957; position: absolute;\n" +
+            "    top: -10px;\n" +
+            "    right: -6px;"
+
+        const popularText = document.createElement("strong")
+        popularText.textContent = "Популярна"
+
+        popularDiv.appendChild(popularText)
+        pizzaCard.appendChild(popularDiv)
+    }
+
+    const imgPizza = document.createElement("img")
+    imgPizza.className = "image"
+    imgPizza.src = data.icon
+    imgPizza.alt = "pizza's photo"
+
+    pizzaCard.appendChild(imgPizza)
+
+    const pizzaItemDiv = document.createElement("div")
+    pizzaItemDiv.className = "pizza-item"
+
+    const pizzaNameH1 = document.createElement("h1")
+    pizzaNameH1.className = "pizza-name"
+
+    const strongPizzaName = document.createElement("strong")
+    strongPizzaName.textContent = data.title
+
+    pizzaNameH1.appendChild(strongPizzaName)
+    pizzaItemDiv.appendChild(pizzaNameH1)
+
+    const pizzaType = document.createElement("h6")
+    pizzaType.className = "pizza-type"
+    pizzaType.textContent = data.type
+    pizzaType.dataset.meat = false
+    pizzaType.dataset.pineapple = false
+    pizzaType.dataset.mushroom = false
+    pizzaType.dataset.seafood = false
+    pizzaType.dataset.vega = data.type === "Вега піца"
+
+    pizzaItemDiv.appendChild(pizzaType)
+
+    const content = document.createElement("p")
+    content.className = "pizza-text"
+    let allContent = ""
+
+    for (const contentKey in data.content) {
+        data.content[contentKey].forEach(item => allContent += item + ", ")
+        if (data.type !== "Вега піца") {
+            if(contentKey === "meat" || contentKey === "chicken"){
+                pizzaType.dataset.meat = true
+            }
+            if(contentKey === "pineapple") {
+                pizzaType.dataset.pineapple = true
+            }
+            if(contentKey === "mushroom") {
+                pizzaType.dataset.mushroom = true
+            }
+            if(contentKey === "ocean") {
+                pizzaType.dataset.seafood = true
+            }
+        }
+    }
+
+    content.textContent = allContent[0].toUpperCase() + allContent.slice(1, allContent.length-2)
+
+    pizzaItemDiv.appendChild(content)
+
+    const pizzaBuyContainers = document.createElement("div")
+    pizzaBuyContainers.className = "pizza-buy-containers"
+
+    if (data.small_size !== undefined) {
+        const pizzaBuyDiv = document.createElement("div")
+        pizzaBuyDiv.className = "pizza-buy-container"
+
+        const sizeDiv = document.createElement("div")
+
+        const imgSize = document.createElement("img")
+        imgSize.alt = "size"
+        imgSize.src = "assets/images/size-icon.svg"
+
+        sizeDiv.appendChild(imgSize)
+
+        const spanSize = document.createElement("span")
+        spanSize.textContent = data.small_size.size
+
+        sizeDiv.appendChild(spanSize)
+
+        pizzaBuyDiv.appendChild(sizeDiv)
+
+        const weightDiv = document.createElement("div")
+
+        const imgWeight = document.createElement("img")
+        imgWeight.alt = "weight"
+        imgWeight.src = "assets/images/weight.svg"
+
+        weightDiv.appendChild(imgWeight)
+
+        const spanWeight = document.createElement("span")
+        spanWeight.textContent = data.small_size.weight
+
+        weightDiv.appendChild(spanWeight)
+
+        pizzaBuyDiv.appendChild(weightDiv)
+
+        const strongPrice = document.createElement("strong")
+        strongPrice.className = "pizza-price"
+        strongPrice.textContent = data.small_size.price
+
+        pizzaBuyDiv.appendChild(strongPrice)
+
+        const justCurrency = document.createElement("strong")
+        justCurrency.textContent = "грн."
+
+        pizzaBuyDiv.appendChild(justCurrency)
+
+        const buyButton = document.createElement("button")
+        buyButton.className = "pizza-buy-button"
+        buyButton.textContent = "Купити"
+        buyButton.type = "submit"
+        buyButton.dataset.size = "small"
+        buyButton.addEventListener("click", addBasketButton)
+
+        pizzaBuyDiv.appendChild(buyButton)
+        pizzaBuyContainers.appendChild(pizzaBuyDiv)
+    }
+    if (data.big_size !== undefined) {
+        const pizzaBuyDiv = document.createElement("div")
+        pizzaBuyDiv.className = "pizza-buy-container"
+
+        const sizeDiv = document.createElement("div")
+
+        const imgSize = document.createElement("img")
+        imgSize.alt = "size"
+        imgSize.src = "assets/images/size-icon.svg"
+
+        sizeDiv.appendChild(imgSize)
+
+        const spanSize = document.createElement("span")
+        spanSize.textContent = data.big_size.size
+
+        sizeDiv.appendChild(spanSize)
+
+        pizzaBuyDiv.appendChild(sizeDiv)
+
+        const weightDiv = document.createElement("div")
+
+        const imgWeight = document.createElement("img")
+        imgWeight.alt = "weight"
+        imgWeight.src = "assets/images/weight.svg"
+
+        weightDiv.appendChild(imgWeight)
+
+        const spanWeight = document.createElement("span")
+        spanWeight.textContent = data.big_size.weight
+
+        weightDiv.appendChild(spanWeight)
+
+        pizzaBuyDiv.appendChild(weightDiv)
+
+        const strongPrice = document.createElement("strong")
+        strongPrice.className = "pizza-price"
+        strongPrice.textContent = data.big_size.price
+
+        pizzaBuyDiv.appendChild(strongPrice)
+
+        const justCurrency = document.createElement("strong")
+        justCurrency.textContent = "грн."
+
+        pizzaBuyDiv.appendChild(justCurrency)
+
+        const buyButton = document.createElement("button")
+        buyButton.className = "pizza-buy-button"
+        buyButton.textContent = "Купити"
+        buyButton.type = "submit"
+        buyButton.dataset.size = "big"
+        buyButton.addEventListener("click", addBasketButton)
+
+        pizzaBuyDiv.appendChild(buyButton)
+        pizzaBuyContainers.appendChild(pizzaBuyDiv)
+    }
+
+    pizzaItemDiv.appendChild(pizzaBuyContainers)
+
+    pizzaCard.appendChild(pizzaItemDiv)
+    document.querySelector(".pizza-allCards").appendChild(pizzaCard)
+}
+
 let basketOrder = []
 
 let a = document.querySelectorAll(".pizza-buy-button")
